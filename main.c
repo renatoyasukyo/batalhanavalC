307e5483eff4ae0062b97f368436e90c32d50212
Renato Yasukyo <nato.kyo@hotmail.com>
2023-06-15 12:27:35 -0300

    versao final

--- main.c
+++ main.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int opcao_menu();
-void menu();
+
+int menu(int matriz1[10][10], int matriz2[10][10]);
 int alocar_navios(int matriz1[10][10], int matriz2[10][10]);
 int navio_jogador1(int matriz[10][10]);
 int navio_jogador2(int matriz[10][10]);
+int ataque_jogador1(int matriz[10][10], int matriz_ataque[10][10], int *ponto1);
+int ataque_jogador2(int matriz[10][10], int matriz_ataque[10][10], int *ponto2);
 int iniciar_jogo(int matriz1[10][10], int matriz2[10][10]);
 void imprimir_matriz(int matriz[10][10]);
 void preencher_intervalo(int matriz[10][10], int linha_inicial, int coluna_inicial, int linha_final, int coluna_final);
 
+
 int main()
 {
+    int matriz1[10][10] = {0};
+    int matriz2[10][10] = {0};
     printf("BATALHA NAVAL POR RENATO YASUKYO\n");
     printf("UNIFESP - SJC\n");
     printf("Pressione a tecla ENTER para continuar: ");
     getchar();
-    menu();
+    menu(matriz1, matriz2);
 }
 
-void menu()
+
+int menu(int matriz1[10][10], int matriz2[10][10])
 {
+    int opc;
     printf("Selecione:\n");
     printf("1. Alocar navios\n");
     printf("2. Iniciar jogo\n");
     printf("3. Sair do jogo\n");
-    opcao_menu();
-}
-
-int opcao_menu()
-{
-    int opc;
-    int matriz1[10][10];
-    int matriz2[10][10];
     scanf("%d", &opc);
     switch (opc)
     {
@@ -41,14 +40,18 @@ int opcao_menu()
         break;
     case 2:
         iniciar_jogo(matriz1, matriz2);
+        menu(matriz1, matriz2);
         break;
     case 3:
+        printf("Finalizando o jogo...");
         return 0;
     default:
         printf("Opcao invalida.\n");
-        menu();
+        menu(matriz1, matriz2);
     }
+    return 0;
 }
+
 
 int alocar_navios(int matriz1[10][10], int matriz2[10][10])
 {
@@ -60,51 +63,44 @@ int alocar_navios(int matriz1[10][10], int matriz2[10][10])
     {
     case 1:
         navio_jogador1(matriz1);
-        menu();
+        menu(matriz1, matriz2);
         break;
     case 2:
         navio_jogador2(matriz2);
-        menu();
+        menu(matriz1, matriz2);
         break;
     default:
         printf("Jogador invalido\n");
-        opcao_menu();
+        menu(matriz1, matriz2);
     }
+    return 0;
 }
 
 int navio_jogador1(int matriz[10][10])
 {
     int linhaInicial, colunaInicial, linhaFinal, colunaFinal;
-    for(int i = 0; i < 10; i ++){
-        for(int j = 0; j < 10; j++){
-            matriz[i][j] = 0;
-        }
-    }
+
     printf("Digite as coordenadas, da menor para a maior, do porta-avioes de 4 casas (exemplo: a1 e depois a4):\n");
     do
     {
-        // Lendo a entrada do usuário
-        char coordenadaInicial[3], coordenadaFinal[3];
-        scanf("%s %s", coordenadaInicial, coordenadaFinal);
+        char coordenadaInicial[4], coordenadaFinal[4]; // Aumentando o tamanho dos arrays para 4 caracteres
+        scanf("%3s %3s", coordenadaInicial, coordenadaFinal); // Limitando a leitura a no máximo 3 caracteres
 
-        // Obtendo as coordenadas em formato de números
-        linhaInicial = coordenadaInicial[1] - '1';
         colunaInicial = coordenadaInicial[0] - 'a';
-        linhaFinal = coordenadaFinal[1] - '1';
         colunaFinal = coordenadaFinal[0] - 'a';
 
-        // Verificando a validade do intervalo
-        if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 3 || colunaInicial - colunaFinal == 3)) ||
-            (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 3 || linhaInicial - linhaFinal == 3)))
+        linhaInicial = atoi(coordenadaInicial + 1) - 1;
+        linhaFinal = atoi(coordenadaFinal + 1) - 1;
+
+        if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 3)) ||
+            (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 3)))
         {
-            // Intervalo válido, preencher com 1
             preencher_intervalo(matriz, linhaInicial, colunaInicial, linhaFinal, colunaFinal);
             imprimir_matriz(matriz);
             break;
         }
         else
         {
-            // Intervalo inválido, solicitar novamente
             printf("Intervalo inválido. Por favor, digite outro intervalo (exemplo: a1-a4):\n");
         }
     } while (1);
@@ -114,28 +110,24 @@ int navio_jogador1(int matriz[10][10])
         printf("Digite as coordenadas, da menor para a maior, do navio-tanque de 3 casas (exemplo: b1 e depois b3):\n");
         do
         {
-            // Lendo a entrada do usuário
-            char coordenadaInicial[3], coordenadaFinal[3];
-            scanf("%s %s", coordenadaInicial, coordenadaFinal);
+            char coordenadaInicial[4], coordenadaFinal[4]; // Aumentando o tamanho dos arrays para 4 caracteres
+            scanf("%3s %3s", coordenadaInicial, coordenadaFinal); // Limitando a leitura a no máximo 3 caracteres
 
-            // Obtendo as coordenadas em formato de números
-            linhaInicial = coordenadaInicial[1] - '1';
             colunaInicial = coordenadaInicial[0] - 'a';
-            linhaFinal = coordenadaFinal[1] - '1';
             colunaFinal = coordenadaFinal[0] - 'a';
 
-            // Verificando a validade do intervalo
-            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 2 || colunaInicial - colunaFinal == 2)) ||
-                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 2 || linhaInicial - linhaFinal == 2)))
+            linhaInicial = atoi(coordenadaInicial + 1) - 1;
+            linhaFinal = atoi(coordenadaFinal + 1) - 1;
+
+            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 2)) ||
+                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 2)))
             {
-                // Intervalo válido, preencher com 1
                 preencher_intervalo(matriz, linhaInicial, colunaInicial, linhaFinal, colunaFinal);
                 imprimir_matriz(matriz);
                 break;
             }
             else
             {
-                // Intervalo inválido, solicitar novamente
                 printf("Intervalo inválido. Por favor, digite outro intervalo (exemplo: b1-b3):\n");
             }
         } while (1);
@@ -146,28 +138,24 @@ int navio_jogador1(int matriz[10][10])
         printf("Digite as coordenadas, da menor para a maior, do contratorpedeiro de 2 casas (exemplo: c1 e depois c2):\n");
         do
         {
-            // Lendo a entrada do usuário
-            char coordenadaInicial[3], coordenadaFinal[3];
-            scanf("%s %s", coordenadaInicial, coordenadaFinal);
+            char coordenadaInicial[4], coordenadaFinal[4]; // Aumentando o tamanho dos arrays para 4 caracteres
+            scanf("%3s %3s", coordenadaInicial, coordenadaFinal); // Limitando a leitura a no máximo 3 caracteres
 
-            // Obtendo as coordenadas em formato de números
-            linhaInicial = coordenadaInicial[1] - '1';
             colunaInicial = coordenadaInicial[0] - 'a';
-            linhaFinal = coordenadaFinal[1] - '1';
             colunaFinal = coordenadaFinal[0] - 'a';
 
-            // Verificando a validade do intervalo
-            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 1 || colunaInicial - colunaFinal == 1)) ||
-                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 1 || linhaInicial - linhaFinal == 1)))
+            linhaInicial = atoi(coordenadaInicial + 1) - 1;
+            linhaFinal = atoi(coordenadaFinal + 1) - 1;
+
+            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 1)) ||
+                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 1)))
             {
-                // Intervalo válido, preencher com 1
                 preencher_intervalo(matriz, linhaInicial, colunaInicial, linhaFinal, colunaFinal);
                 imprimir_matriz(matriz);
                 break;
             }
             else
             {
-                // Intervalo inválido, solicitar novamente
                 printf("Intervalo inválido. Por favor, digite outro intervalo (exemplo: c1-c2):\n");
             }
         } while (1);
@@ -178,15 +166,15 @@ int navio_jogador1(int matriz[10][10])
         printf("Digite as coordenadas do submarino de 1 casa (exemplo: d1):\n");
         do
         {
-            char coordenada[3];
-            scanf("%s", coordenada);
+            char coordenada[4];
+            scanf("%3s", coordenada);
 
-            linhaFinal = coordenada[1] - '1';
+            linhaFinal = atoi(coordenada + 1) - 1;
             colunaFinal = coordenada[0] - 'a';
 
             if (linhaFinal >= 0 && linhaFinal < 10 && colunaFinal >= 0 && colunaFinal < 10)
             {
-                matriz[linhaFinal][colunaFinal] = 1;
+                matriz[linhaFinal][colunaFinal] = 2;
                 imprimir_matriz(matriz);
                 break;
             }
@@ -202,36 +190,28 @@ int navio_jogador1(int matriz[10][10])
 int navio_jogador2(int matriz[10][10])
 {
     int linhaInicial, colunaInicial, linhaFinal, colunaFinal;
-    for(int i = 0; i < 10; i ++){
-        for(int j = 0; j < 10; j++){
-            matriz[i][j] = 0;
-        }
-    }
+
     printf("Digite as coordenadas, da menor para a maior, do porta-avioes de 4 casas (exemplo: a1 e depois a4):\n");
     do
     {
-        // Lendo a entrada do usuário
-        char coordenadaInicial[3], coordenadaFinal[3];
-        scanf("%s %s", coordenadaInicial, coordenadaFinal);
+        char coordenadaInicial[4], coordenadaFinal[4]; // Aumentando o tamanho dos arrays para 4 caracteres
+        scanf("%3s %3s", coordenadaInicial, coordenadaFinal); // Limitando a leitura a no máximo 3 caracteres
 
-        // Obtendo as coordenadas em formato de números
-        linhaInicial = coordenadaInicial[1] - '1';
         colunaInicial = coordenadaInicial[0] - 'a';
-        linhaFinal = coordenadaFinal[1] - '1';
         colunaFinal = coordenadaFinal[0] - 'a';
 
-        // Verificando a validade do intervalo
-        if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 3 || colunaInicial - colunaFinal == 3)) ||
-            (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 3 || linhaInicial - linhaFinal == 3)))
+        linhaInicial = atoi(coordenadaInicial + 1) - 1;
+        linhaFinal = atoi(coordenadaFinal + 1) - 1;
+
+        if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 3)) ||
+            (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 3)))
         {
-            // Intervalo válido, preencher com 1
             preencher_intervalo(matriz, linhaInicial, colunaInicial, linhaFinal, colunaFinal);
             imprimir_matriz(matriz);
             break;
         }
         else
         {
-            // Intervalo inválido, solicitar novamente
             printf("Intervalo inválido. Por favor, digite outro intervalo (exemplo: a1-a4):\n");
         }
     } while (1);
@@ -241,28 +221,24 @@ int navio_jogador2(int matriz[10][10])
         printf("Digite as coordenadas, da menor para a maior, do navio-tanque de 3 casas (exemplo: b1 e depois b3):\n");
         do
         {
-            // Lendo a entrada do usuário
-            char coordenadaInicial[3], coordenadaFinal[3];
-            scanf("%s %s", coordenadaInicial, coordenadaFinal);
+            char coordenadaInicial[4], coordenadaFinal[4]; // Aumentando o tamanho dos arrays para 4 caracteres
+            scanf("%3s %3s", coordenadaInicial, coordenadaFinal); // Limitando a leitura a no máximo 3 caracteres
 
-            // Obtendo as coordenadas em formato de números
-            linhaInicial = coordenadaInicial[1] - '1';
             colunaInicial = coordenadaInicial[0] - 'a';
-            linhaFinal = coordenadaFinal[1] - '1';
             colunaFinal = coordenadaFinal[0] - 'a';
 
-            // Verificando a validade do intervalo
-            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 2 || colunaInicial - colunaFinal == 2)) ||
-                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 2 || linhaInicial - linhaFinal == 2)))
+            linhaInicial = atoi(coordenadaInicial + 1) - 1;
+            linhaFinal = atoi(coordenadaFinal + 1) - 1;
+
+            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 2)) ||
+                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 2)))
             {
-                // Intervalo válido, preencher com 1
                 preencher_intervalo(matriz, linhaInicial, colunaInicial, linhaFinal, colunaFinal);
                 imprimir_matriz(matriz);
                 break;
             }
             else
             {
-                // Intervalo inválido, solicitar novamente
                 printf("Intervalo inválido. Por favor, digite outro intervalo (exemplo: b1-b3):\n");
             }
         } while (1);
@@ -273,28 +249,24 @@ int navio_jogador2(int matriz[10][10])
         printf("Digite as coordenadas, da menor para a maior, do contratorpedeiro de 2 casas (exemplo: c1 e depois c2):\n");
         do
         {
-            // Lendo a entrada do usuário
-            char coordenadaInicial[3], coordenadaFinal[3];
-            scanf("%s %s", coordenadaInicial, coordenadaFinal);
+            char coordenadaInicial[4], coordenadaFinal[4]; // Aumentando o tamanho dos arrays para 4 caracteres
+            scanf("%3s %3s", coordenadaInicial, coordenadaFinal); // Limitando a leitura a no máximo 3 caracteres
 
-            // Obtendo as coordenadas em formato de números
-            linhaInicial = coordenadaInicial[1] - '1';
             colunaInicial = coordenadaInicial[0] - 'a';
-            linhaFinal = coordenadaFinal[1] - '1';
             colunaFinal = coordenadaFinal[0] - 'a';
 
-            // Verificando a validade do intervalo
-            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 1 || colunaInicial - colunaFinal == 1)) ||
-                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 1 || linhaInicial - linhaFinal == 1)))
+            linhaInicial = atoi(coordenadaInicial + 1) - 1;
+            linhaFinal = atoi(coordenadaFinal + 1) - 1;
+
+            if ((linhaInicial == linhaFinal && (colunaFinal - colunaInicial == 1)) ||
+                (colunaInicial == colunaFinal && (linhaFinal - linhaInicial == 1)))
             {
-                // Intervalo válido, preencher com 1
                 preencher_intervalo(matriz, linhaInicial, colunaInicial, linhaFinal, colunaFinal);
                 imprimir_matriz(matriz);
                 break;
             }
             else
             {
-                // Intervalo inválido, solicitar novamente
                 printf("Intervalo inválido. Por favor, digite outro intervalo (exemplo: c1-c2):\n");
             }
         } while (1);
@@ -305,15 +277,15 @@ int navio_jogador2(int matriz[10][10])
         printf("Digite as coordenadas do submarino de 1 casa (exemplo: d1):\n");
         do
         {
-            char coordenada[3];
-            scanf("%s", coordenada);
+            char coordenada[4];
+            scanf("%3s", coordenada);
 
-            linhaFinal = coordenada[1] - '1';
+            linhaFinal = atoi(coordenada + 1) - 1;
             colunaFinal = coordenada[0] - 'a';
 
             if (linhaFinal >= 0 && linhaFinal < 10 && colunaFinal >= 0 && colunaFinal < 10)
             {
-                matriz[linhaFinal][colunaFinal] = 1;
+                matriz[linhaFinal][colunaFinal] = 2;
                 imprimir_matriz(matriz);
                 break;
             }
@@ -328,25 +300,25 @@ int navio_jogador2(int matriz[10][10])
 
 void imprimir_matriz(int matriz[10][10])
 {
-    printf("\nMatriz resultante:\n");
-
-    printf("\t");
+    printf("\n");
+    printf("   ");
     for (int coluna = 0; coluna < 10; coluna++)
     {
-        printf("%c\t", 'A' + coluna);
+        printf("%c ", 'A' + coluna);
     }
     printf("\n");
 
     for (int linha = 0; linha < 10; linha++)
     {
-        printf("%d\t", linha + 1);
+        printf("%2d ", linha + 1);
 
         for (int coluna = 0; coluna < 10; coluna++)
         {
-            printf("%d\t", matriz[linha][coluna]);
+            printf("%d ", matriz[linha][coluna]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 void preencher_intervalo(int matriz[10][10], int linha_inicial, int coluna_inicial, int linha_final, int coluna_final)
@@ -355,14 +327,85 @@ void preencher_intervalo(int matriz[10][10], int linha_inicial, int coluna_inici
     {
         for (int coluna = coluna_inicial; coluna <= coluna_final; coluna++)
         {
-            matriz[linha][coluna] = 1;
+            matriz[linha][coluna] = 2;
         }
     }
 }
 
 int iniciar_jogo(int matriz1[10][10], int matriz2[10][10])
 {
-    imprimir_matriz(matriz1);
-    imprimir_matriz(matriz2);
+    int matriz_ataque1[10][10] = {0};
+    int matriz_ataque2[10][10] = {0};
+    int ponto1 = 0;
+    int ponto2 = 0;
+    do
+    {
+        ataque_jogador1(matriz1, matriz_ataque1, &ponto1);
+        ataque_jogador2(matriz2, matriz_ataque2, &ponto2);
+    }while(ponto1 < 20 && ponto2 < 20);
+    if(ponto1 = 20){
+        printf("O JOGADOR 1 VENCEU!!!\n\n\n");
+    }
+    else if(ponto2 = 20){
+        printf("O JOGADOR 2 VENCEU!!!\n\n\n");
+    }
+    return 0;
+}
+
+int ataque_jogador1(int matriz[10][10], int matriz_ataque[10][10], int *ponto1)
+{
+    printf("\nJogador 1, ataque.\n");
+    int linha;
+    char coluna;
+    printf("Digite a coordenada do ataque (exemplo: a1): ");
+    char coordenada[4];
+    scanf("%3s", coordenada);
+
+    linha = atoi(coordenada + 1) - 1;
+    coluna = coordenada[0] - 'a';
+
+    if (matriz_ataque[linha][coluna] == 1 || matriz_ataque[linha][coluna] == 2) {
+        printf("Posicao ja foi atacada.\n");
+    }
+    else if (matriz[linha][coluna] == 2) {
+        printf("Acertou!\n");
+        matriz_ataque[linha][coluna] = 2;
+        (*ponto1)++;
+    }
+    else {
+        printf("Errou!\n");
+        matriz_ataque[linha][coluna] = 1;
+    }
+    imprimir_matriz(matriz_ataque);
+    printf("\n");
+    return 0;
+}
+
+int ataque_jogador2(int matriz[10][10], int matriz_ataque[10][10], int *ponto2)
+{
+    printf("\nJogador 2, ataque.\n");
+    int linha;
+    char coluna;
+    printf("Digite a coordenada do ataque (exemplo: a1): ");
+    char coordenada[4];
+    scanf("%3s", coordenada);
+
+    linha = atoi(coordenada + 1) - 1;
+    coluna = coordenada[0] - 'a';
+
+    if (matriz_ataque[linha][coluna] == 1 || matriz_ataque[linha][coluna] == 2) {
+        printf("Posicao ja foi atacada.\n");
+    }
+    else if (matriz[linha][coluna] == 2) {
+        printf("Acertou!\n");
+        matriz_ataque[linha][coluna] = 2;
+        (*ponto2)++;
+    }
+    else {
+        printf("Errou!\n");
+        matriz_ataque[linha][coluna] = 1;
+    }
+    imprimir_matriz(matriz_ataque);
+    printf("\n");
     return 0;
 }
